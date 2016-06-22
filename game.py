02e2e24f7180423cf7f6e6a36c411948d422bccc
makaimark <makaimark@gmail.com>
2016-06-22 22:57:49 +0200

    first ready version

--- game.py
+++ game.py
@@ -1,49 +1,22 @@
-import math
 import random
-a = []
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-a.append(random.randint(1, 99))
-for i in range(10):
-    g = int(input("Enter an integer from 1 to 99: "))
-    while a[i] != g:
-        if g < a[i]:
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 99: "))
-        elif g > a[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 99: "))
-        else:
-            break
-    print("you guessed it!")
 
-b = []
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-b.append(random.randint(1, 49))
-for i in range(10):
-    g = int(input("Enter an integer from 1 to 49: "))
-    while b[i] != g:
-        if g < b[i]:
-            print("guess is low")
-            g = int(input("Enter an integer from 1 to 49: "))
-        elif g > b[i]:
-            print("guess is high")
-            g = int(input("Enter an integer from 1 to 49: "))
-        else:
-            break
-    print("you guessed it!")
+
+def print_logic(input_int, rand_num):
+    if input_int < rand_num:
+        return "guess is low"
+    elif input_int > rand_num:
+        return "guess is high"
+    else:
+        return "you guessed it!"
+
+
+def game_logic(integer):
+    for i in range(10):
+        input_int = 0
+        random_num = random.randint(1, integer)
+        while random_num != input_int:
+            input_int = int(input("Enter an integer from 1 to " + str(integer) + ":"))
+            print(print_logic(input_int, random_num))
+
+game_logic(99)
+game_logic(49)
