488f2a18c246e766f070810c574725b80dff88cb
makaimark <makaimark@gmail.com>
2016-06-26 15:09:19 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,22 +1,14 @@
 import random
 
 
-def print_logic(input_int, rand_num):
-    if input_int < rand_num:
-        return "guess is low"
-    elif input_int > rand_num:
-        return "guess is high"
-    else:
-        return "you guessed it!"
-
-
 def game_logic(integer):
-    for i in range(10):
-        input_int = 0
-        random_num = random.randint(1, integer)
-        while random_num != input_int:
-            input_int = int(input("Enter an integer from 1 to " + str(integer) + ":"))
-            print(print_logic(input_int, random_num))
+    for i in range(10):             # The game contains 10 turn
+        input_int = 0               # In every new turn we change the value to zero
+        rand_num = random.randint(1, integer)     # We generate a random number between 1 and 99\49
+        while rand_num != input_int:              # While we don't find the input_int
+            input_int = int(input("Enter an integer from 1 to " + str(integer) + ":"))  # Ask a new number from the user
+            print("guess is low" if input_int < rand_num else "guess is high" if input_int > rand_num else "you got it")
+            # Compare input_int and rand_num, then print the result
 
-game_logic(99)
-game_logic(49)
+game_logic(99)      # First part of the game, random nums between 1 and 99
+game_logic(49)      # Second part of the game, random nums between 1 and 49
